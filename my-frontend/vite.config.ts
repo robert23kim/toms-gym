347b3cbad5895777b4dc1f1e43abac61b8205dc9
Tom Oka <toka778@example.com>
2025-03-23 20:42:45 -0400

    front end working

---
+++ my-frontend/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(async ({ mode }) => {
+  const plugins = [react()];
+
+  if (mode === "development") {
+    const { componentTagger } = await import("lovable-tagger");
+    plugins.push(componentTagger());
+  }
+
+  return {
+    server: {
+      host: "::",
+      port: 8080,
+    },
+    plugins,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
+
